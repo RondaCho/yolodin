d9738a303fc7fab9dc0074640d35a6ec9e9623ac
RondaCho <duswls9292@naver.com>
2018-04-20 11:58:07 +0900

    settings.py에 allowed_host 추가

--- yolodin/settings.py
+++ yolodin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yzt^7$=#$@bp7#glpenr+rn&y+ja!l$g_nz%$q+f5ay%(c9g)+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'yolodin.pythonanywhere.com']
 
 
 # Application definition
